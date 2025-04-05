99396482eaaa43c4fae733dc7436c711ee0ef7b0
M.F.M <mfmfazrin1986@gmail.com>
2025-04-05 03:52:53 +0300

    style: remove commented-out code in Hero component

--- src/components/cms-hosting/Hero.tsx
+++ src/components/cms-hosting/Hero.tsx
@@ -165,7 +165,6 @@ const Hero = () => {
                 }}
               ></div>
               
-              {/* Video element replacing the image */}
             </motion.div>
           </motion.div>
         </div>
